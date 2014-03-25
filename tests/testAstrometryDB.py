949aa0aaad8a20df21713185528cc829ea82515c
danielsf <scott.f.daniel@gmail.com>
2014-03-25 10:30:47 -0700

    testAstrometryDB.py runs

--- tests/testAstrometryDB.py
+++ tests/testAstrometryDB.py
@@ -15,6 +15,35 @@ from lsst.sims.catalogs.generation.db import DBObject, ObservationMetaData
 from lsst.sims.catalogs.measures.instance import InstanceCatalog
 import lsst.sims.catalogs.generation.utils.testUtils as tu
 
+"""
+class sfdStarClass(DBObject):
+    objid = 'msstars'
+    tableid = 'stars'
+    idColKey = 'id'
+    raColName = 'ra'
+    decColName = 'decl'
+    objectTypeId = 4
+    spatialModel = 'POINT'
+    dbAddress = 'sqlite:///testDatabase.db'
+    dbDefaultValues = {'varsimobjid':-1, 'runid':-1, 'ismultiple':-1, 'run':-1,
+                       'runobjid':-1}
+    #These types should be matched to the database.
+    #: Default map is float.  If the column mapping is the same as the column name, None can be specified
+    columns = [('id',None, int),
+               ('raJ2000', 'ra*%f'%(numpy.pi/180.)),
+               ('decJ2000', 'decl*%f'%(numpy.pi/180.)),
+               #('glon', 'gal_l*%f'%(numpy.pi/180.)),
+               #('glat', 'gal_b*%f'%(numpy.pi/180.)),
+               #('magNorm', '(-2.5*log(flux_scale)/log(10.)) - 18.402732642'),
+               #('properMotionRa', '(mudecl/(1000.*3600.))*PI()/180.'),
+               #('properMotionDec', '(mura/(1000.*3600.))*PI()/180.'),
+               #('galacticAv', 'CONVERT(float, ebv*3.1)'),
+               #('radialVelocity', 'vrad'),
+               ('variabilityParameters', 'varParamStr', str, 256),
+               ('sedFilename', 'sedfilename', unicode, 40)]
+
+"""
+
 class sfdTestDB(DBObject):
     objid = 'teststars'
     tableid = 'stars'
@@ -35,9 +64,8 @@ class sfdTestDB(DBObject):
                ('zmag', None),
                ('ymag', None),
                ('mag_norm', None)]
-    
-               #('ra_corr',None),
-               #('dec_corr',None)]
+
+
 
 class sfdTestCatalog(InstanceCatalog,Astrometry,Photometry):
     catalog_type = 'sfd_test'
@@ -45,9 +73,14 @@ class sfdTestCatalog(InstanceCatalog,Astrometry,Photometry):
 
 sfd_db=sfdTestDB()
 print 'key is ',sfd_db.getIdColKey()
-
-#result=sfd_db.query_columns()
-#tu.writeResult(result,"sfd_db.sav")
-
 sfd_cat=sfdTestCatalog(sfd_db)
 sfd_cat.write_catalog("sfd_catalog_output.sav")
+
+
+"""
+sfd_stars=sfdStarClass()
+
+#sfd_stars=DBObject.from_objid('msstars')
+result=sfd_stars.query_columns()
+tu.writeResult(restul,"sfd_star_db.sav")
+"""
