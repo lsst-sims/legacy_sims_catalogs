a7b55b47d80d40e5916e52b8b115508a87797e1d
danielsf <scott.f.daniel@gmail.com>
2014-07-22 10:11:12 -0700

    trying to figure out how to put SQRT into the sqlite databases

--- tests/testCatalogs.py
+++ tests/testCatalogs.py
@@ -3,6 +3,7 @@ import os
 import numpy
 import unittest
 import eups
+import sqlalchemy
 import lsst.utils.tests as utilsTests
 from lsst.sims.catalogs.generation.db import DBObject, ObservationMetaData
 from lsst.sims.catalogs.measures.instance import InstanceCatalog, compound
@@ -41,14 +42,35 @@ def compareFiles(file1, file2):
 
 class InstanceCatalogTestCase(unittest.TestCase):
     def setUp(self):
+        
         if os.path.exists('testDatabase.db'):
             os.unlink('testDatabase.db')
         tu.makeStarTestDB(size=100000, seedVal=1)
         tu.makeGalTestDB(size=100000, seedVal=1)
+                
         self.obsMd = ObservationMetaData(circ_bounds=dict(ra=210., dec=-60, radius=1.75), mjd=52000.,
                                                bandpassName='r')
         self.mystars = DBObject.from_objid('teststars')
+        
+        conn=self.mystars.engine.raw_connection()
+        conn.create_function("COS",1,numpy.cos)
+        conn.create_function("SIN",1,numpy.sin)
+        conn.create_function("ASIN",1,numpy.arcsin)
+        conn.create_function("SQRT",1,numpy.sqrt)
+        
+        #this will let me play around here in real time
+        #see if I can execute a query using one of these functions
+        import pdb
+        pdb.set_trace()
+        
         self.mygals = DBObject.from_objid('testgals')
+        
+        gconn=self.mygals.engine.raw_connection()
+        gconn.create_function("COS",1,numpy.cos)
+        gconn.create_function("SIN",1,numpy.sin)
+        gconn.create_function("ASIN",1,numpy.arcsin)
+        gconn.create_function("SQRT",1,numpy.sqrt)
+        
         self.basedir = eups.productDir('sims_catalogs_measures')+"/tests/"
 
     def tearDown(self):
@@ -76,12 +98,55 @@ class InstanceCatalogTestCase(unittest.TestCase):
         self.assertTrue(compareFiles('test_BASIC.out', self.basedir+'testdata/BASIC_GAL.out'))
         os.unlink('test_BASIC.out')
 
+class boundingBoxTest(unittest.TestCase):
+    def setUp(self):
+        
+        self.RAmin = 200.
+        self.RAmax = 215.
+        self.DECmin = -70.
+        self.DECmax = -50.
+        
+        self.RAcenter = 200.
+        self.DECcenter = -60.
+        self.radius = 1.75
+        
+        if os.path.exists('testDatabase.db'):
+            os.unlink('testDatabase.db')
+        tu.makeStarTestDB(size=100000, seedVal=1)
+        self.obsMdCirc = ObservationMetaData(
+                         circ_bounds=dict(ra=self.RAcenter, dec=self.DECcenter, radius=self.radius), 
+                         mjd=52000., bandpassName='r')
+                         
+        self.obsMdBox = ObservationMetaData(box_bounds=dict(ra_min=self.RAmin,ra_max=self.RAmax,
+                                            dec_min=self.DECmin,dec_max=self.DECmax),
+                                            mjd=52000., bandpassName='r')
+                                            
+        self.mystars = DBObject.from_objid('teststars')
+        
+        #self.basedir = eups.productDir('sims_catalogs_measures')+"/tests/"
+
+    def tearDown(self):
+        del self.obsMdCirc
+        del self.obsMdBox
+        del self.mystars
+
+    def testBoxBounds(self):
+        myCatalog = self.mystars.getCatalog('basic_catalog',obs_metadata = self.obsMdBox)
+        myIterator = myCatalog.iter_catalog(chunk_size=10)
+        
+        for line in myIterator:
+            print line
+        
+        myCatalog.write_catalog("myCatalog_junk.sav")
+    
+
 def suite():
     """Returns a suite containing all the test cases in this module."""
     utilsTests.init()
     suites = []
     suites += unittest.makeSuite(InstanceCatalogTestCase)
     suites += unittest.makeSuite(utilsTests.MemoryTestCase)
+    #suites += unittest.makeSuite(boundingBoxTest)
 
     return unittest.TestSuite(suites)
 
