6cefe9252d6bae3dad1ebdd67f96256e81ef375c
danielsf <scott.f.daniel@gmail.com>
2014-07-28 14:36:34 -0700

    now we also test the completeness of the bounds

--- tests/testCatalogs.py
+++ tests/testCatalogs.py
@@ -9,6 +9,15 @@ from lsst.sims.catalogs.generation.db import DBObject, ObservationMetaData
 from lsst.sims.catalogs.measures.instance import InstanceCatalog, compound
 import lsst.sims.catalogs.generation.utils.testUtils as tu
 
+class BoundsCatalog(InstanceCatalog):
+    catalog_type = 'bounds_catalog'
+    refIdCol = 'id'
+    column_outputs = ['id', 'raJ2000', 'decJ2000']
+    transformations = {'raJ2000':numpy.degrees,
+                       'decJ2000':numpy.degrees}
+    
+    default_formats = {'f':'%.20f'}
+
 class BasicCatalog(InstanceCatalog):
     catalog_type = 'basic_catalog'
     refIdCol = 'id'
@@ -102,7 +111,7 @@ def controlHaversine(ra1deg,dec1deg,ra2deg,dec2deg):
     arg = (numpy.sin(0.5*(dec1-dec2)))**2+numpy.cos(dec1)*numpy.cos(dec2)*(numpy.sin(0.5*(ra1-ra2)))**2
     rr = 2.0*numpy.arcsin(numpy.sqrt(arg))
         
-    return rr
+    return rr/conversion
 
 class boundingBoxTest(unittest.TestCase):
     def setUp(self):
@@ -143,7 +152,7 @@ class boundingBoxTest(unittest.TestCase):
         Make sure that box_bound_constraint in sims.catalogs.generation.db.dbConnection.py
         does not admit any objects outside of the bounding box
         """
-        myCatalog = self.mystars.getCatalog('basic_catalog',obs_metadata = self.obsMdBox)
+        myCatalog = self.mystars.getCatalog('bounds_catalog',obs_metadata = self.obsMdBox)
 
         myIterator = myCatalog.iter_catalog(chunk_size=10)
         
@@ -153,22 +162,100 @@ class boundingBoxTest(unittest.TestCase):
             self.assertTrue(line[2]>self.DECmin)
             self.assertTrue(line[2]<self.DECmax)
        
+        myCatalog.write_catalog('box_test_catalog.txt')
         
+        #now we will test for the completeness of the box bounds 
+        obsMdControl = ObservationMetaData(box_bounds=dict(ra_min = self.RAcenter-20.0, 
+                                                ra_max = self.RAcenter+20.0,
+                                                dec_min = self.DECcenter-20.0, 
+                                                dec_max = self.DECcenter+20.0),
+                                                mjd=52000.0, bandpassName = 'r')
+      
+        controlCatalog = self.mystars.getCatalog('bounds_catalog',obs_metadata = obsMdControl)
+        controlCatalog.write_catalog('box_control_catalog.txt')
+        
+        ftest = open('box_test_catalog.txt')
+        idtest = []
+        for longline in ftest:
+            line = longline.split()    
+            
+            #note that catalogs are output with commas at the end of values  
+            idtest.append(line[0][:-1])
+        ftest.close()
+        
+        fcontrol = open('box_control_catalog.txt')
+        for longline in fcontrol:
+            line = longline.split()
+            if line[0] != '#id,':
+                #note that catalogs are output with commas at the end of values
+                idcontrol = line[0][:-1]
+                racontrol = float(line[1][:-1])
+                deccontrol = float(line[2][:-1])
+                
+                if racontrol < self.RAmax and racontrol > self.RAmin \
+                and deccontrol < self.DECmax and deccontrol > self.DECmin:
+                    
+                    self.assertTrue(idcontrol in idtest)
+        
+        fcontrol.close()
+        os.unlink('box_control_catalog.txt')
+        os.unlink('box_test_catalog.txt')
+        
+
     def testCircBounds(self):
         
         """
         Make sure that circular_bound_constraint in sims.catalogs.generation.db.dbConnection.py
         does not admit any objects outside of the bounding circle
         """
-        
-        myCatalog = self.mystars.getCatalog('basic_catalog',obs_metadata = self.obsMdCirc)
+
+        myCatalog = self.mystars.getCatalog('bounds_catalog',obs_metadata = self.obsMdCirc)
         myIterator = myCatalog.iter_catalog(chunk_size=10)
     
         for line in myIterator:
             rtest = controlHaversine(self.RAcenter, self.DECcenter, line[1], line[2])
             self.assertTrue(rtest<self.radius)
-        
+       
+        myCatalog.write_catalog('circular_test_catalog.txt')
+       
+        #now we will test for the completeness of the circular bounds 
+        obsMdControl = ObservationMetaData(box_bounds=dict(ra_min = self.RAcenter-20.0, 
+                                                ra_max = self.RAcenter+20.0,
+                                                dec_min = self.DECcenter-20.0, 
+                                                dec_max = self.DECcenter+20.0),
+                                                mjd=52000.0, bandpassName = 'r')
+      
+        controlCatalog = self.mystars.getCatalog('bounds_catalog',obs_metadata = obsMdControl)
+        controlCatalog.write_catalog('circular_control_catalog.txt')
+       
+        ftest = open('circular_test_catalog.txt')
+        idtest = []
+        for longline in ftest:
+            line = longline.split()
+            
+            #note that the catalogs are output with commas at the end of values
 
+            idtest.append(line[0][:-1])
+        ftest.close()
+
+       
+        fcontrol = open('circular_control_catalog.txt')
+        for longline in fcontrol:
+            line=longline.split()
+            if line[0]!='#id,':
+                
+                #note that the catalogs are output with commas at the end of values
+                idcontrol = line[0][:-1]
+                racontrol = float(line[1][:-1])
+                deccontrol = float(line[2][:-1])
+                rr = controlHaversine(self.RAcenter, self.DECcenter, racontrol, deccontrol)
+                
+                if rr < self.radius:
+                    self.assertTrue(idcontrol in idtest)
+               
+        fcontrol.close()
+        os.unlink('circular_control_catalog.txt')
+        os.unlink('circular_test_catalog.txt')
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
