1ce5f30df7a8d966686c997c13a08bfe91ffef6f
danielsf <scott.f.daniel@gmail.com>
2016-08-26 15:08:30 -0700

    pep8 changes to testUtils.py

--- python/lsst/sims/catalogs/utils/testUtils.py
+++ python/lsst/sims/catalogs/utils/testUtils.py
@@ -1,24 +1,27 @@
 import os
 import sqlite3
-from collections import OrderedDict
-from numpy.random import random, seed
-import numpy, json
+from numpy.random import random
+from numpy.random import seed
+import numpy
+import json
 
 from lsst.sims.utils import ObservationMetaData
 from lsst.sims.catalogs.db import CatalogDBObject
-from lsst.sims.utils import _raDecFromAltAz, _getRotSkyPos, _getRotTelPos, Site, \
-                            raDecFromAltAz, haversine
+from lsst.sims.utils import (_raDecFromAltAz, _getRotSkyPos, _getRotTelPos, Site,
+                             raDecFromAltAz, haversine)
 
 __all__ = ["getOneChunk", "writeResult", "sampleSphere", "myTestGals",
            "makeGalTestDB", "myTestStars", "makeStarTestDB", "makePhoSimTestDB"]
+
 
 def getOneChunk(results):
     try:
         chunk = results.next()
     except StopIteration:
-        raise RuntimeError("No results were returned.  Cannot run tests.  Try increasing the size of the"+
+        raise RuntimeError("No results were returned.  Cannot run tests.  Try increasing the size of the"
                            " test database")
     return chunk
+
 
 def writeResult(result, fname):
     fh = open(fname, 'w')
@@ -31,14 +34,16 @@ def writeResult(result, fname):
             fh.write(",".join([str(chunk[name][i]) for name in chunk.dtype.names])+"\n")
     fh.close()
 
+
 def sampleSphere(size, ramin = 0., dra = 2.*numpy.pi):
-    #From Shao 1996: "Spherical Sampling by Archimedes' Theorem"
+    # From Shao 1996: "Spherical Sampling by Archimedes' Theorem"
     ra = random(size)*dra
     ra += ramin
     ra %= 2*numpy.pi
     z = random(size)*2. - 1.
     dec = numpy.arccos(z) - numpy.pi/2.
     return ra, dec
+
 
 def sampleFocus(size, raCenter, decCenter, radius):
     """
@@ -60,11 +65,12 @@ def sampleFocus(size, raCenter, decCenter, radius):
 
     return ra, dec
 
+
 class myTestGals(CatalogDBObject):
     objid = 'testgals'
     tableid = 'galaxies'
     idColKey = 'id'
-    #Make this implausibly large?
+    # Make this implausibly large?
     appendint = 1022
     objectTypeId = 45
     driver = 'sqlite'
@@ -88,10 +94,11 @@ class myTestGals(CatalogDBObject):
                ('a_disk', None),
                ('b_disk', None),
                ('a_bulge', None),
-               ('b_bulge', None),]
+               ('b_bulge', None)]
+
 
 def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
-    raCenter=None, decCenter=None, radius=None, **kwargs):
+                  raCenter=None, decCenter=None, radius=None, **kwargs):
     """
     Make a test database to serve information to the myTestGals object
     @param size: Number of rows in the database
@@ -107,10 +114,10 @@ def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     c = conn.cursor()
     try:
         c.execute('''CREATE TABLE galaxies
-                     (id int, ra real, decl real, umag real, gmag real, rmag real,
-                     imag real, zmag real, ymag real,
-                     mag_norm_agn real, mag_norm_bulge real, mag_norm_disk real,
-                     redshift real, a_disk real, b_disk real, a_bulge real, b_bulge real, varParamStr text)''')
+                  (id int, ra real, decl real, umag real, gmag real, rmag real,
+                  imag real, zmag real, ymag real,
+                  mag_norm_agn real, mag_norm_bulge real, mag_norm_disk real,
+                  redshift real, a_disk real, b_disk real, a_bulge real, b_bulge real, varParamStr text)''')
         conn.commit()
     except:
         raise RuntimeError("Error creating database.")
@@ -122,9 +129,8 @@ def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     else:
         rc = numpy.radians(raCenter)
         dc = numpy.radians(decCenter)
-        rr = numpy.radians(radius)
         ra, dec = sampleFocus(size, rc, dc, radius)
-    #Typical colors for main sequece stars
+    # Typical colors for main sequece stars
     umg = 1.5
     gmr = 0.65
     rmi = 1.0
@@ -136,14 +142,14 @@ def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     redshift = random(size)*2.5
 
     a_disk = random(size)*2.
-    flatness = random(size)*0.8 # To prevent linear galaxies
+    flatness = random(size)*0.8  # To prevent linear galaxies
     b_disk = a_disk*(1 - flatness)
 
     a_bulge = random(size)*1.5
     flatness = random(size)*0.5
     b_bulge = a_bulge*(1 - flatness)
 
-    #assume mag norm is g-band (which is close to true)
+    # assume mag norm is g-band (which is close to true)
     mag_norm = -2.5*numpy.log10(numpy.power(10, mag_norm_disk/-2.5) + numpy.power(10, mag_norm_bulge/-2.5) +
                                 numpy.power(10, mag_norm_agn/-2.5))
     umag = mag_norm + umg
@@ -155,25 +161,28 @@ def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     for i in xrange(size):
         period = random()*490. + 10.
         amp = random()*5. + 0.2
-        varParam = {'varMethodName':'testVar', 'pars':{'period':period, 'amplitude':amp}}
+        varParam = {'varMethodName': 'testVar', 'pars': {'period': period, 'amplitude': amp}}
         paramStr = json.dumps(varParam)
         qstr = '''INSERT INTO galaxies VALUES (%i, %f, %f, %f,
                      %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f,
-                     %f, %f, '%s')'''%\
-                   (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i], imag[i],
-                   zmag[i], ymag[i], mag_norm_agn[i], mag_norm_bulge[i], mag_norm_disk[i], redshift[i],
-                   a_disk[i], b_disk[i], a_bulge[i], b_bulge[i], paramStr)
+                     %f, %f, '%s')''' % \
+               (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i], imag[i],
+                zmag[i], ymag[i], mag_norm_agn[i], mag_norm_bulge[i], mag_norm_disk[i], redshift[i],
+                a_disk[i], b_disk[i], a_bulge[i], b_bulge[i], paramStr)
+
         c.execute(qstr)
+
     c.execute('''CREATE INDEX gal_ra_idx ON galaxies (ra)''')
     c.execute('''CREATE INDEX gal_dec_idx ON galaxies (decl)''')
     conn.commit()
     conn.close()
 
+
 class myTestStars(CatalogDBObject):
     objid = 'teststars'
     tableid = 'stars'
     idColKey = 'id'
-    #Make this implausibly large?
+    # Make this implausibly large?
     appendint = 1023
     objectTypeId = 46
     driver = 'sqlite'
@@ -194,8 +203,9 @@ class myTestStars(CatalogDBObject):
                ('ymag', None),
                ('magNorm', 'mag_norm', float)]
 
+
 def makeStarTestDB(filename='testDatabase.db', size=1000, seedVal=None,
-    raCenter=None, decCenter=None, radius=None, **kwargs):
+                   raCenter=None, decCenter=None, radius=None, **kwargs):
     """
     Make a test database to serve information to the myTestStars object
     @param size: Number of rows in the database
@@ -226,17 +236,16 @@ def makeStarTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     else:
         rc = numpy.radians(raCenter)
         dc = numpy.radians(decCenter)
-        rr = numpy.radians(radius)
         ra, dec = sampleFocus(size, rc, dc, radius)
 
-    #Typical colors
+    # Typical colors
     umg = 1.5
     gmr = 0.65
     rmi = 1.0
     imz = 0.45
     zmy = 0.3
     mag_norm = random(size)*6. + 18.
-    #assume mag norm is g-band (which is close to true)
+    # assume mag norm is g-band (which is close to true)
     umag = mag_norm + umg
     gmag = mag_norm
     rmag = gmag - gmr
@@ -244,23 +253,27 @@ def makeStarTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     zmag = imag - imz
     ymag = zmag - zmy
     radVel = random(size)*50. - 25.
-    pmRa = random(size)*4./(1000*3600.) # deg/yr
-    pmDec = random(size)*4./(1000*3600.) # deg/yr
-    parallax = random(size)*1.0 #milliarcseconds per year
+    pmRa = random(size)*4./(1000*3600.)  # deg/yr
+    pmDec = random(size)*4./(1000*3600.)  # deg/yr
+    parallax = random(size)*1.0  # milliarcseconds per year
     for i in xrange(size):
         period = random()*490. + 10.
         amp = random()*5. + 0.2
-        varParam = {'varMethodName':'testVar', 'pars':{'period':period, 'amplitude':amp}}
+        varParam = {'varMethodName': 'testVar', 'pars': {'period': period, 'amplitude': amp}}
         paramStr = json.dumps(varParam)
-        qstr = '''INSERT INTO stars VALUES (%i, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %.15f, %.15f, %.15f, '%s')'''%\
-                  (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i],
-                   imag[i], zmag[i], ymag[i], mag_norm[i], radVel[i], pmRa[i], pmDec[i], parallax[i],
-                   paramStr)
+        qstr = '''INSERT INTO stars VALUES (%i, %f, %f, %f, %f, %f, %f,
+               %f, %f, %f, %f, %.15f, %.15f, %.15f, '%s')''' % \
+               (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i],
+                imag[i], zmag[i], ymag[i], mag_norm[i], radVel[i], pmRa[i], pmDec[i], parallax[i],
+                paramStr)
+
         c.execute(qstr)
+
     c.execute('''CREATE INDEX star_ra_idx ON stars (ra)''')
     c.execute('''CREATE INDEX star_dec_idx ON stars (decl)''')
     conn.commit()
     conn.close()
+
 
 def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, radius=0.1,
                      displacedRA=None, displacedDec=None,
@@ -298,23 +311,24 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     if os.path.exists(filename):
         os.unlink(filename)
 
-    #just an example of some valid SED file names
-    galaxy_seds = ['Const.80E07.02Z.spec','Inst.80E07.002Z.spec','Burst.19E07.0005Z.spec']
+    # just an example of some valid SED file names
+    galaxy_seds = ['Const.80E07.02Z.spec', 'Inst.80E07.002Z.spec', 'Burst.19E07.0005Z.spec']
     agn_sed = 'agn.spec'
-    star_seds = ['km20_5750.fits_g40_5790','m2.0Full.dat','bergeron_6500_85.dat_6700']
+    star_seds = ['km20_5750.fits_g40_5790', 'm2.0Full.dat', 'bergeron_6500_85.dat_6700']
 
     numpy.random.seed(seedVal)
 
     if displacedRA is not None and displacedDec is not None:
         if len(displacedRA) != len(displacedDec):
-            raise RuntimeError("WARNING in makePhoSimTestDB displacedRA and displacedDec have different lengths")
+            raise RuntimeError("WARNING in makePhoSimTestDB displacedRA and "
+                               "displacedDec have different lengths")
 
     if displacedRA is not None:
         size = len(displacedRA)
     elif displacedDec is not None:
         size = len(displacedDec)
 
-    #create the ObservationMetaData object
+    # create the ObservationMetaData object
     mjd = 52000.0
     alt = numpy.pi/2.0
     az = 0.0
@@ -348,8 +362,8 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
                                   'dist2moon': dist2moon,
                                   'rottelpos': numpy.degrees(rotTel)}
 
-    #Now begin building the database.
-    #First create the tables.
+    # Now begin building the database.
+    # First create the tables.
     conn = sqlite3.connect(filename)
     c = conn.cursor()
     try:
@@ -393,11 +407,12 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     try:
         c.execute('''CREATE TABLE StarAllForceseek
                   (simobjid int, ra real, decl real, magNorm real,
-                  mudecl real, mura real, galacticAv real, vrad real, varParamStr text, sedFilename text, parallax real)''')
+                  mudecl real, mura real, galacticAv real, vrad real, varParamStr text,
+                  sedFilename text, parallax real)''')
     except:
         raise RuntimeError("Error creating StarAllForceseek table.")
 
-    #Now generate the data to be stored in the tables.
+    # Now generate the data to be stored in the tables.
 
     rr = numpy.random.sample(size)*numpy.radians(radius)
     theta = numpy.random.sample(size)*2.0*numpy.pi
@@ -406,7 +421,6 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
         ra = numpy.degrees(centerRA + rr*numpy.cos(theta))
     else:
         ra = numpy.degrees(centerRA) + displacedRA
-
 
     if displacedDec is None:
         dec = numpy.degrees(centerDec + rr*numpy.sin(theta))
@@ -444,7 +458,7 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     r_ab = numpy.random.sample(size)*4.0 + 17.0
     i_ab = numpy.random.sample(size)*4.0 + 17.0
     z_ab = numpy.random.sample(size)*4.0 + 17.0
-    y_ab = numpy.random.sample(size)*4.0 +17.0
+    y_ab = numpy.random.sample(size)*4.0 + 17.0
     redshift = numpy.random.sample(size)*2.0
 
     t0_mjd = mjd - numpy.random.sample(size)*1000.0
@@ -482,21 +496,23 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     period = numpy.random.sample(size)*20.0
     amp = numpy.random.sample(size)*5.0
 
-    #write the data to the tables.
+    # write the data to the tables.
     for i in range(size):
 
         cmd = '''INSERT INTO galaxy_bulge VALUES (%i, %i, %f, %f, %f, %f, %f,
-                     '%s', %f, %f, %f, %i, '%s', %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)''' %\
-                     (i, i, bra[i], bdec[i], ra[i], dec[i], magnorm_bulge[i], galaxy_seds[i%len(galaxy_seds)],
-                     a_b[i], b_b[i], pa_bulge[i], 4, 'CCM', av_b[i], rv_b[i], u_ab[i], g_ab[i],
-                     r_ab[i], i_ab[i], z_ab[i], y_ab[i], redshift[i], BulgeHalfLightRadius[i])
+              '%s', %f, %f, %f, %i, '%s', %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)''' % \
+              (i, i, bra[i], bdec[i], ra[i], dec[i], magnorm_bulge[i], galaxy_seds[i%len(galaxy_seds)],
+               a_b[i], b_b[i], pa_bulge[i], 4, 'CCM', av_b[i], rv_b[i], u_ab[i], g_ab[i],
+               r_ab[i], i_ab[i], z_ab[i], y_ab[i], redshift[i], BulgeHalfLightRadius[i])
+
         c.execute(cmd)
 
-        varParam = {'varMethodName':'applyAgn',
-                    'pars':{'agn_tau':round(agn_tau[i],4), 't0_mjd':round(t0_mjd[i],4),
-                    'agn_sfu':round(agn_sfu[i],4), 'agn_sfg':round(agn_sfg[i],4), 'agn_sfr':round(agn_sfr[i],4),
-                    'agn_sfi':round(agn_sfi[i],4), 'agn_sfz':round(agn_sfz[i],4), 'agn_sfy':round(agn_sfy[i],4),
-                    'seed':int(agnSeed[i])}}
+        varParam = {'varMethodName': 'applyAgn',
+                    'pars': {'agn_tau': round(agn_tau[i], 4), 't0_mjd': round(t0_mjd[i], 4),
+                             'agn_sfu': round(agn_sfu[i], 4), 'agn_sfg': round(agn_sfg[i], 4),
+                             'agn_sfr': round(agn_sfr[i], 4), 'agn_sfi': round(agn_sfi[i], 4),
+                             'agn_sfz': round(agn_sfz[i], 4), 'agn_sfy': round(agn_sfy[i], 4),
+                             'seed': int(agnSeed[i])}}
 
         paramStr = json.dumps(varParam)
 
@@ -507,32 +523,33 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
                                             '%s', %f, %f,
                                             '%s', %f, %f,
                                             %f, %f, %f, %f, %f, %f,
-                                            %f, %f, %f)''' %\
-                     (i, i, ra[i], dec[i], bra[i], bdec[i], dra[i], ddec[i], agnra[i], agndec[i],
-                     magnorm_bulge[i], magnorm_disk[i], magnorm_agn[i],
-                     galaxy_seds[i%len(galaxy_seds)], galaxy_seds[i%len(galaxy_seds)], agn_sed,
-                     paramStr,
-                     a_b[i], b_b[i], pa_bulge[i], 4,
-                     a_d[i], b_d[i], pa_disk[i], 1,
-                     'CCM', av_b[i], rv_b[i],
-                     'CCM', av_d[i], rv_d[i],
-                     u_ab[i], g_ab[i], r_ab[i], i_ab[i], z_ab[i], y_ab[i], redshift[i],
-                     BulgeHalfLightRadius[i], DiskHalfLightRadius[i])
+                                            %f, %f, %f)''' % \
+              (i, i, ra[i], dec[i], bra[i], bdec[i], dra[i], ddec[i], agnra[i], agndec[i],
+               magnorm_bulge[i], magnorm_disk[i], magnorm_agn[i],
+               galaxy_seds[i%len(galaxy_seds)], galaxy_seds[i%len(galaxy_seds)], agn_sed,
+               paramStr,
+               a_b[i], b_b[i], pa_bulge[i], 4,
+               a_d[i], b_d[i], pa_disk[i], 1,
+               'CCM', av_b[i], rv_b[i],
+               'CCM', av_d[i], rv_d[i],
+               u_ab[i], g_ab[i], r_ab[i], i_ab[i], z_ab[i], y_ab[i], redshift[i],
+               BulgeHalfLightRadius[i], DiskHalfLightRadius[i])
         c.execute(cmd)
 
         cmd = '''INSERT INTO galaxy_agn VALUES (%i, %i, %f, %f, %f, %f, %f, '%s', '%s',
-                                               %f, %f, %f, %f, %f, %f, %f)''' %\
-                                               (i, i, agnra[i], agndec[i], ra[i], dec[i],
-                                               magnorm_agn[i], agn_sed, paramStr,
-                                               u_ab[i], g_ab[i], r_ab[i], i_ab[i],
-                                               z_ab[i], y_ab[i], redshift[i])
+              %f, %f, %f, %f, %f, %f, %f)''' % \
+              (i, i, agnra[i], agndec[i], ra[i], dec[i],
+               magnorm_agn[i], agn_sed, paramStr,
+               u_ab[i], g_ab[i], r_ab[i], i_ab[i],
+               z_ab[i], y_ab[i], redshift[i])
+
         c.execute(cmd)
 
-        varParam = {'varMethodName':'testVar', 'pars':{'period':period[i], 'amplitude':amp[i]}}
+        varParam = {'varMethodName': 'testVar', 'pars': {'period': period[i], 'amplitude': amp[i]}}
         paramStr = json.dumps(varParam)
         cmd = '''INSERT INTO StarAllForceseek VALUES (%i, %f, %f, %f, %f, %f, %f, %f, '%s', '%s', %f)''' %\
-                  (i, raStar[i], decStar[i], magnormStar[i], mudecl[i], mura[i],
-                  galacticAv[i], vrad[i], paramStr, star_seds[i%len(star_seds)], parallax[i])
+              (i, raStar[i], decStar[i], magnormStar[i], mudecl[i], mura[i],
+               galacticAv[i], vrad[i], paramStr, star_seds[i%len(star_seds)], parallax[i])
 
         c.execute(cmd)
 
