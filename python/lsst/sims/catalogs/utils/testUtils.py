66e035a0d3cf89fbf45816dfba37276915343d81
danielsf <scott.f.daniel@gmail.com>
2016-08-26 15:28:05 -0700

    import numpy as np in testUtils.py; replace np.random with np.RandomState

--- python/lsst/sims/catalogs/utils/testUtils.py
+++ python/lsst/sims/catalogs/utils/testUtils.py
@@ -1,8 +1,6 @@
 import os
 import sqlite3
-from numpy.random import random
-from numpy.random import seed
-import numpy
+import numpy as np
 import json
 
 from lsst.sims.utils import ObservationMetaData
@@ -35,33 +33,40 @@ def writeResult(result, fname):
     fh.close()
 
 
-def sampleSphere(size, ramin = 0., dra = 2.*numpy.pi):
+def sampleSphere(size, ramin = 0., dra = 2.*np.pi, rng=None):
     # From Shao 1996: "Spherical Sampling by Archimedes' Theorem"
-    ra = random(size)*dra
+    if rng is None:
+        rng = np.random.RandomState(42)
+
+    ra = rng.random_sample(size)*dra
     ra += ramin
-    ra %= 2*numpy.pi
-    z = random(size)*2. - 1.
-    dec = numpy.arccos(z) - numpy.pi/2.
+    ra %= 2*np.pi
+    z = rng.random_sample(size)*2. - 1.
+    dec = np.arccos(z) - np.pi/2.
     return ra, dec
 
 
-def sampleFocus(size, raCenter, decCenter, radius):
+def sampleFocus(size, raCenter, decCenter, radius, rng=None):
     """
     Sample points in a focused field of view
     @param [in] raCenter is the RA at the center of the field of view in radians
     @param [in] decCenter is the Dec at the center of the field of view in radians
     @param [in] radius is the radius of the field of view in radians
+    @param [in] rng is a random number generator (an instance of np.random.RandomState)
     @param [out] returns numpy arrays of ra and decs in radians
     """
-    theta = numpy.random.sample(size)
-    rc = numpy.radians(raCenter)
-    dc = numpy.radians(decCenter)
-    rr = numpy.radians(radius)*numpy.random.sample(size)
-    ra = numpy.empty(size)
-    dec = numpy.empty(size)
+    if rng is None:
+        rng = np.random.RandomState(1453)
+
+    theta = rng.random_sample(size)
+    rc = np.radians(raCenter)
+    dc = np.radians(decCenter)
+    rr = np.radians(radius)*rng.random_sample(size)
+    ra = np.empty(size)
+    dec = np.empty(size)
     for i, th in enumerate(theta):
-        ra[i] = rc + rr*numpy.cos(th)
-        dec[i] = dc + rr*numpy.sin(th)
+        ra[i] = rc + rr*np.cos(th)
+        dec[i] = dc + rr*np.sin(th)
 
     return ra, dec
 
@@ -79,8 +84,8 @@ class myTestGals(CatalogDBObject):
     decColName = 'decl'
     spatialModel = 'SERSIC2D'
     columns = [('id', None, int),
-               ('raJ2000', 'ra*%f'%(numpy.pi/180.)),
-               ('decJ2000', 'decl*%f'%(numpy.pi/180.)),
+               ('raJ2000', 'ra*%f'%(np.pi/180.)),
+               ('decJ2000', 'decl*%f'%(np.pi/180.)),
                ('umag', None),
                ('gmag', None),
                ('rmag', None),
@@ -121,37 +126,41 @@ def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
         conn.commit()
     except:
         raise RuntimeError("Error creating database.")
-    if seedVal:
-        seed(seedVal)
+
+    if seedVal is not None:
+        rng = np.random.RandomState(seedVal)
+    else:
+        rng = np.random.RandomState(3321)
 
     if raCenter is None or decCenter is None or radius is None:
-        ra, dec = sampleSphere(size, **kwargs)
+
+        ra, dec = sampleSphere(size, rng=rng, **kwargs)
     else:
-        rc = numpy.radians(raCenter)
-        dc = numpy.radians(decCenter)
-        ra, dec = sampleFocus(size, rc, dc, radius)
+        rc = np.radians(raCenter)
+        dc = np.radians(decCenter)
+        ra, dec = sampleFocus(size, rc, dc, radius, rng=rng)
     # Typical colors for main sequece stars
     umg = 1.5
     gmr = 0.65
     rmi = 1.0
     imz = 0.45
     zmy = 0.3
-    mag_norm_disk = random(size)*6. + 18.
-    mag_norm_bulge = random(size)*6. + 18.
-    mag_norm_agn = random(size)*6. + 19.
-    redshift = random(size)*2.5
+    mag_norm_disk = rng.random_sample(size)*6. + 18.
+    mag_norm_bulge = rng.random_sample(size)*6. + 18.
+    mag_norm_agn = rng.random_sample(size)*6. + 19.
+    redshift = rng.random_sample(size)*2.5
 
-    a_disk = random(size)*2.
-    flatness = random(size)*0.8  # To prevent linear galaxies
+    a_disk = rng.random_sample(size)*2.
+    flatness = rng.random_sample(size)*0.8  # To prevent linear galaxies
     b_disk = a_disk*(1 - flatness)
 
-    a_bulge = random(size)*1.5
-    flatness = random(size)*0.5
+    a_bulge = rng.random_sample(size)*1.5
+    flatness = rng.random_sample(size)*0.5
     b_bulge = a_bulge*(1 - flatness)
 
     # assume mag norm is g-band (which is close to true)
-    mag_norm = -2.5*numpy.log10(numpy.power(10, mag_norm_disk/-2.5) + numpy.power(10, mag_norm_bulge/-2.5) +
-                                numpy.power(10, mag_norm_agn/-2.5))
+    mag_norm = -2.5*np.log10(np.power(10, mag_norm_disk/-2.5) + np.power(10, mag_norm_bulge/-2.5) +
+                             np.power(10, mag_norm_agn/-2.5))
     umag = mag_norm + umg
     gmag = mag_norm
     rmag = gmag - gmr
@@ -159,14 +168,14 @@ def makeGalTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     zmag = imag - imz
     ymag = zmag - zmy
     for i in xrange(size):
-        period = random()*490. + 10.
-        amp = random()*5. + 0.2
+        period = rng.random_sample(1)[0]*490. + 10.
+        amp = rng.random_sample(1)[0]*5. + 0.2
         varParam = {'varMethodName': 'testVar', 'pars': {'period': period, 'amplitude': amp}}
         paramStr = json.dumps(varParam)
         qstr = '''INSERT INTO galaxies VALUES (%i, %f, %f, %f,
                      %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f,
                      %f, %f, '%s')''' % \
-               (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i], imag[i],
+               (i, np.degrees(ra[i]), np.degrees(dec[i]), umag[i], gmag[i], rmag[i], imag[i],
                 zmag[i], ymag[i], mag_norm_agn[i], mag_norm_bulge[i], mag_norm_disk[i], redshift[i],
                 a_disk[i], b_disk[i], a_bulge[i], b_bulge[i], paramStr)
 
@@ -190,11 +199,11 @@ class myTestStars(CatalogDBObject):
     raColName = 'ra'
     decColName = 'decl'
     columns = [('id', None, int),
-               ('raJ2000', 'ra*%f'%(numpy.pi/180.)),
-               ('decJ2000', 'decl*%f'%(numpy.pi/180.)),
-               ('parallax', 'parallax*%.15f'%(numpy.pi/(648000000.0))),
-               ('properMotionRa', 'properMotionRa*%.15f'%(numpy.pi/180)),
-               ('properMotionDec', 'properMotionDec*%.15f'%(numpy.pi/180.)),
+               ('raJ2000', 'ra*%f'%(np.pi/180.)),
+               ('decJ2000', 'decl*%f'%(np.pi/180.)),
+               ('parallax', 'parallax*%.15f'%(np.pi/(648000000.0))),
+               ('properMotionRa', 'properMotionRa*%.15f'%(np.pi/180)),
+               ('properMotionDec', 'properMotionDec*%.15f'%(np.pi/180.)),
                ('umag', None),
                ('gmag', None),
                ('rmag', None),
@@ -228,15 +237,18 @@ def makeStarTestDB(filename='testDatabase.db', size=1000, seedVal=None,
         conn.commit()
     except:
         raise RuntimeError("Error creating database.")
-    if seedVal:
-        seed(seedVal)
+
+    if seedVal is not None:
+        rng = np.random.RandomState(seedVal)
+    else:
+        rng = np.random.RandomState(88)
 
     if raCenter is None or decCenter is None or radius is None:
-        ra, dec = sampleSphere(size, **kwargs)
+        ra, dec = sampleSphere(size, rng=rng, **kwargs)
     else:
-        rc = numpy.radians(raCenter)
-        dc = numpy.radians(decCenter)
-        ra, dec = sampleFocus(size, rc, dc, radius)
+        rc = np.radians(raCenter)
+        dc = np.radians(decCenter)
+        ra, dec = sampleFocus(size, rc, dc, radius, rng=rng)
 
     # Typical colors
     umg = 1.5
@@ -244,7 +256,7 @@ def makeStarTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     rmi = 1.0
     imz = 0.45
     zmy = 0.3
-    mag_norm = random(size)*6. + 18.
+    mag_norm = rng.random_sample(size)*6. + 18.
     # assume mag norm is g-band (which is close to true)
     umag = mag_norm + umg
     gmag = mag_norm
@@ -252,18 +264,18 @@ def makeStarTestDB(filename='testDatabase.db', size=1000, seedVal=None,
     imag = rmag - rmi
     zmag = imag - imz
     ymag = zmag - zmy
-    radVel = random(size)*50. - 25.
-    pmRa = random(size)*4./(1000*3600.)  # deg/yr
-    pmDec = random(size)*4./(1000*3600.)  # deg/yr
-    parallax = random(size)*1.0  # milliarcseconds per year
+    radVel = rng.random_sample(size)*50. - 25.
+    pmRa = rng.random_sample(size)*4./(1000*3600.)  # deg/yr
+    pmDec = rng.random_sample(size)*4./(1000*3600.)  # deg/yr
+    parallax = rng.random_sample(size)*1.0  # milliarcseconds per year
     for i in xrange(size):
-        period = random()*490. + 10.
-        amp = random()*5. + 0.2
+        period = rng.random_sample(1)[0]*490. + 10.
+        amp = rng.random_sample(1)[0]*5. + 0.2
         varParam = {'varMethodName': 'testVar', 'pars': {'period': period, 'amplitude': amp}}
         paramStr = json.dumps(varParam)
         qstr = '''INSERT INTO stars VALUES (%i, %f, %f, %f, %f, %f, %f,
                %f, %f, %f, %f, %.15f, %.15f, %.15f, '%s')''' % \
-               (i, numpy.degrees(ra[i]), numpy.degrees(dec[i]), umag[i], gmag[i], rmag[i],
+               (i, np.degrees(ra[i]), np.degrees(dec[i]), umag[i], gmag[i], rmag[i],
                 imag[i], zmag[i], ymag[i], mag_norm[i], radVel[i], pmRa[i], pmDec[i], parallax[i],
                 paramStr)
 
@@ -316,7 +328,7 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     agn_sed = 'agn.spec'
     star_seds = ['km20_5750.fits_g40_5790', 'm2.0Full.dat', 'bergeron_6500_85.dat_6700']
 
-    numpy.random.seed(seedVal)
+    rng = np.random.RandomState(seedVal)
 
     if displacedRA is not None and displacedDec is not None:
         if len(displacedRA) != len(displacedDec):
@@ -330,7 +342,7 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
 
     # create the ObservationMetaData object
     mjd = 52000.0
-    alt = numpy.pi/2.0
+    alt = np.pi/2.0
     az = 0.0
 
     testSite = Site(name='LSST')
@@ -339,9 +351,9 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     rotTel = _getRotTelPos(centerRA, centerDec, obsTemp, 0.0)
     rotSkyPos = _getRotSkyPos(centerRA, centerDec, obsTemp, rotTel)
 
-    obs_metadata = ObservationMetaData(pointingRA=numpy.degrees(centerRA),
-                                       pointingDec=numpy.degrees(centerDec),
-                                       rotSkyPos=numpy.degrees(rotSkyPos),
+    obs_metadata = ObservationMetaData(pointingRA=np.degrees(centerRA),
+                                       pointingDec=np.degrees(centerDec),
+                                       rotSkyPos=np.degrees(rotSkyPos),
                                        bandpassName=bandpass,
                                        mjd=mjd,
                                        boundType = 'circle', boundLength = 2.0*radius,
@@ -352,7 +364,7 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
     sun_alt = -90.0
 
     moon_ra, moon_dec = raDecFromAltAz(moon_alt, 0.0, obs_metadata)
-    dist2moon = haversine(numpy.radians(moon_ra), numpy.radians(moon_dec),
+    dist2moon = haversine(np.radians(moon_ra), np.radians(moon_dec),
                           obs_metadata._pointingRA, obs_metadata._pointingDec)
 
     obs_metadata.OpsimMetaData = {'moonra': moon_ra,
@@ -360,7 +372,7 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
                                   'moonalt': moon_alt,
                                   'sunalt': sun_alt,
                                   'dist2moon': dist2moon,
-                                  'rottelpos': numpy.degrees(rotTel)}
+                                  'rottelpos': np.degrees(rotTel)}
 
     # Now begin building the database.
     # First create the tables.
@@ -414,87 +426,87 @@ def makePhoSimTestDB(filename='PhoSimTestDatabase.db', size=1000, seedVal=32, ra
 
     # Now generate the data to be stored in the tables.
 
-    rr = numpy.random.sample(size)*numpy.radians(radius)
-    theta = numpy.random.sample(size)*2.0*numpy.pi
+    rr = rng.random_sample(size)*np.radians(radius)
+    theta = rng.random_sample(size)*2.0*np.pi
 
     if displacedRA is None:
-        ra = numpy.degrees(centerRA + rr*numpy.cos(theta))
+        ra = np.degrees(centerRA + rr*np.cos(theta))
     else:
-        ra = numpy.degrees(centerRA) + displacedRA
+        ra = np.degrees(centerRA) + displacedRA
 
     if displacedDec is None:
-        dec = numpy.degrees(centerDec + rr*numpy.sin(theta))
+        dec = np.degrees(centerDec + rr*np.sin(theta))
     else:
-        dec = numpy.degrees(centerDec) + displacedDec
+        dec = np.degrees(centerDec) + displacedDec
 
-    bra = numpy.radians(ra+numpy.random.sample(size)*0.01*radius)
-    bdec = numpy.radians(dec+numpy.random.sample(size)*0.01*radius)
-    dra = numpy.radians(ra + numpy.random.sample(size)*0.01*radius)
-    ddec = numpy.radians(dec + numpy.random.sample(size)*0.01*radius)
-    agnra = numpy.radians(ra + numpy.random.sample(size)*0.01*radius)
-    agndec = numpy.radians(dec + numpy.random.sample(size)*0.01*radius)
+    bra = np.radians(ra+rng.random_sample(size)*0.01*radius)
+    bdec = np.radians(dec+rng.random_sample(size)*0.01*radius)
+    dra = np.radians(ra + rng.random_sample(size)*0.01*radius)
+    ddec = np.radians(dec + rng.random_sample(size)*0.01*radius)
+    agnra = np.radians(ra + rng.random_sample(size)*0.01*radius)
+    agndec = np.radians(dec + rng.random_sample(size)*0.01*radius)
 
-    magnorm_bulge = numpy.random.sample(size)*4.0 + 17.0
-    magnorm_disk = numpy.random.sample(size)*5.0 + 17.0
-    magnorm_agn = numpy.random.sample(size)*5.0 + 17.0
-    b_b = numpy.random.sample(size)*0.2
-    a_b = b_b+numpy.random.sample(size)*0.05
-    b_d = numpy.random.sample(size)*0.5
-    a_d = b_d+numpy.random.sample(size)*0.1
+    magnorm_bulge = rng.random_sample(size)*4.0 + 17.0
+    magnorm_disk = rng.random_sample(size)*5.0 + 17.0
+    magnorm_agn = rng.random_sample(size)*5.0 + 17.0
+    b_b = rng.random_sample(size)*0.2
+    a_b = b_b+rng.random_sample(size)*0.05
+    b_d = rng.random_sample(size)*0.5
+    a_d = b_d+rng.random_sample(size)*0.1
 
-    BulgeHalfLightRadius = numpy.random.sample(size)*0.2
-    DiskHalfLightRadius = numpy.random.sample(size)*0.5
+    BulgeHalfLightRadius = rng.random_sample(size)*0.2
+    DiskHalfLightRadius = rng.random_sample(size)*0.5
 
-    pa_bulge = numpy.random.sample(size)*360.0
-    pa_disk = numpy.random.sample(size)*360.0
+    pa_bulge = rng.random_sample(size)*360.0
+    pa_disk = rng.random_sample(size)*360.0
 
-    av_b = numpy.random.sample(size)*0.4
-    av_d = numpy.random.sample(size)*0.4
-    rv_b = numpy.random.sample(size)*0.1 + 3.0
-    rv_d = numpy.random.sample(size)*0.1 + 3.0
+    av_b = rng.random_sample(size)*0.4
+    av_d = rng.random_sample(size)*0.4
+    rv_b = rng.random_sample(size)*0.1 + 3.0
+    rv_d = rng.random_sample(size)*0.1 + 3.0
 
-    u_ab = numpy.random.sample(size)*4.0 + 17.0
-    g_ab = numpy.random.sample(size)*4.0 + 17.0
-    r_ab = numpy.random.sample(size)*4.0 + 17.0
-    i_ab = numpy.random.sample(size)*4.0 + 17.0
-    z_ab = numpy.random.sample(size)*4.0 + 17.0
-    y_ab = numpy.random.sample(size)*4.0 + 17.0
-    redshift = numpy.random.sample(size)*2.0
+    u_ab = rng.random_sample(size)*4.0 + 17.0
+    g_ab = rng.random_sample(size)*4.0 + 17.0
+    r_ab = rng.random_sample(size)*4.0 + 17.0
+    i_ab = rng.random_sample(size)*4.0 + 17.0
+    z_ab = rng.random_sample(size)*4.0 + 17.0
+    y_ab = rng.random_sample(size)*4.0 + 17.0
+    redshift = rng.random_sample(size)*2.0
 
-    t0_mjd = mjd - numpy.random.sample(size)*1000.0
-    agn_tau = numpy.random.sample(size)*1000.0 + 1000.0
-    agnSeed = numpy.random.random_integers(low=2, high=4000, size=size)
-    agn_sfu = numpy.random.sample(size)
-    agn_sfg = numpy.random.sample(size)
-    agn_sfr = numpy.random.sample(size)
-    agn_sfi = numpy.random.sample(size)
-    agn_sfz = numpy.random.sample(size)
-    agn_sfy = numpy.random.sample(size)
+    t0_mjd = mjd - rng.random_sample(size)*1000.0
+    agn_tau = rng.random_sample(size)*1000.0 + 1000.0
+    agnSeed = rng.random_integers(low=2, high=4000, size=size)
+    agn_sfu = rng.random_sample(size)
+    agn_sfg = rng.random_sample(size)
+    agn_sfr = rng.random_sample(size)
+    agn_sfi = rng.random_sample(size)
+    agn_sfz = rng.random_sample(size)
+    agn_sfy = rng.random_sample(size)
 
-    rrStar = numpy.random.sample(size)*numpy.radians(radius)
-    thetaStar = numpy.random.sample(size)*2.0*numpy.pi
+    rrStar = rng.random_sample(size)*np.radians(radius)
+    thetaStar = rng.random_sample(size)*2.0*np.pi
 
     if displacedRA is None:
-        raStar = centerRA + rrStar*numpy.cos(thetaStar)
+        raStar = centerRA + rrStar*np.cos(thetaStar)
     else:
-        raStar = centerRA + numpy.radians(displacedRA)
+        raStar = centerRA + np.radians(displacedRA)
 
     if displacedDec is None:
-        decStar = centerDec + rrStar*numpy.sin(thetaStar)
+        decStar = centerDec + rrStar*np.sin(thetaStar)
     else:
-        decStar = centerDec + numpy.radians(displacedDec)
+        decStar = centerDec + np.radians(displacedDec)
 
-    raStar = numpy.degrees(raStar)
-    decStar = numpy.degrees(decStar)
+    raStar = np.degrees(raStar)
+    decStar = np.degrees(decStar)
 
-    magnormStar = numpy.random.sample(size)*4.0 + 17.0
-    mudecl = numpy.random.sample(size)*0.0001
-    mura = numpy.random.sample(size)*0.0001
-    galacticAv = numpy.random.sample(size)*0.05*3.1
-    vrad = numpy.random.sample(size)*1.0
-    parallax = 0.00045+numpy.random.sample(size)*0.00001
-    period = numpy.random.sample(size)*20.0
-    amp = numpy.random.sample(size)*5.0
+    magnormStar = rng.random_sample(size)*4.0 + 17.0
+    mudecl = rng.random_sample(size)*0.0001
+    mura = rng.random_sample(size)*0.0001
+    galacticAv = rng.random_sample(size)*0.05*3.1
+    vrad = rng.random_sample(size)*1.0
+    parallax = 0.00045+rng.random_sample(size)*0.00001
+    period = rng.random_sample(size)*20.0
+    amp = rng.random_sample(size)*5.0
 
     # write the data to the tables.
     for i in range(size):
