6729ca61a62ab6cad5eb08f102475759690302ea
danielsf <scott.f.daniel@gmail.com>
2016-12-01 16:15:25 -0800

    include objid in the error message when parallelCatalogWriter fails

--- python/lsst/sims/catalogs/definitions/ParallelCatalogWriter.py
+++ python/lsst/sims/catalogs/definitions/ParallelCatalogWriter.py
@@ -65,7 +65,8 @@ def parallelCatalogWriter(catalog_dict, chunk_size=None, constraint=None,
                        + 'host: %s != %s\n' % (cat.db_obj.connection.host, ref_cat.db_obj.connection.host)
                        + 'port: %s != %s\n' % (cat.db_obj.connection.port, ref_cat.db_obj.connection.port)
                        + 'driver: %s != %s\n' % (cat.db_obj.connection.driver, ref_cat.db_obj.connection.driver)
-                       + 'table: %s != %s\n' % (cat.db_obj.tableid, ref_cat.db_obj.tableid))
+                       + 'table: %s != %s\n' % (cat.db_obj.tableid, ref_cat.db_obj.tableid)
+                       + 'objid: %s !- %s\n' % (cat.db_obj.objid, ref_cat.db_obj.objid))
 
                 raise RuntimeError(msg)
 
